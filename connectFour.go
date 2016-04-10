0b6a32c6b6d4247bd08490f919cd25ade314448d
Max Eshleman <Max@Maxs-MacBook-Pro-2.local>
2016-04-10 15:45:55 -0600

    added web shtuff

--- connectFour.go
+++ connectFour.go
@@ -9,9 +9,32 @@ import (
     "runtime"
     "sync"
     "time"
+    "net/http"
+    "log"
 )
 
+const (
+    DEFAULT_PORT = "9000"
+)
+  
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintln(w, "Hello, World!\n")
+}
+  
 func main() {
+    var port string
+    if port = os.Getenv("PORT"); len(port) == 0 {
+        log.Printf("Warning, PORT not set. Defaulting to %+v\n", DEFAULT_PORT)
+        port = DEFAULT_PORT
+    }
+  
+    http.HandleFunc("/", HelloServer)
+    err := http.ListenAndServe(":" + port, nil)
+    if err != nil {
+        log.Printf("ListenAndServe: ", err)
+    }
+
+
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	// Get command line input
